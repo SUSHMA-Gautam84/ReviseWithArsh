db3728f2c2d0583abaa8ce30796b861f032e829a
SUSHMA-Gautam84 <79377368+SUSHMA-Gautam84@users.noreply.github.com>
2023-01-07 17:39:09 +0530

    Create Factorial with Trailing Zeroes.cpp

---
+++ q3/Factorial with Trailing Zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+         int cnt=0;
+        while(n){
+            n/=5;
+            cnt+=n;
+        }
+        return cnt;
+        
+    }
+};
+
